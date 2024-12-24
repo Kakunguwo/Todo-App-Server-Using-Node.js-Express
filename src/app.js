f52e29aabf53838057086621e79accd4d3d5736f
Ronnie Kakunguwo <ronniekakunguwo03@gmail.com>
2024-12-24 02:57:07 +0200

    `feat: added express-rate-limit and helmet middleware`

--- src/app.js
+++ src/app.js
@@ -3,11 +3,24 @@ import cors from "cors";
 import { errorHandler, notFould } from "./middlewares/errror.middleware.js";
 import todoRoutes from "./routes/todo.route.js";
 import userRoutes from "./routes/user.route.js";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  // store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(limiter);
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/todos", todoRoutes);
